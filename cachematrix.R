8c0f9739075a8d71ef85ed661eb8a8857e14b8d9
vidsubscribe <vidsubscribe@yahoo.com>
2016-09-09 23:06:34 -0500

    Updated by Vidyadhar to add code to handle inverse matrix. This code is tested by passing 2x2 matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+makeCacheMatrix <- function(x = matrix()) 
+  {
+           xinverse<-NULL
+           set<-function(matrixtobereversed) 
+                {x<<-matrixtobereversed
+                 xinverse<<-NULL
+                }
+           get<-function() x
+           setinverse<-function(matrixinverse) xinverse<<-matrixinverse
+           getinverse<-function() xinverse
+           list(set = set, get = get,
+                setinverse = setinverse,
+                getinverse = getinverse)
+  }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inversedmatrix<-x$getinverse()
+  if (!is.null(inversedmatrix))
+  {
+    message("getting cached inverse matrix")
+    return(inversedmatrix)
+  }
+  data<-x$get()
+  inversedmatrix<-solve(data)
+  x$setinverse(inversedmatrix)
+  inversedmatrix
+  
 }
